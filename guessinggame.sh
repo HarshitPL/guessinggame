655bf0c93949be9b522390ebd2ae9437cea99e30
HarshitPL <43979905+HarshitPL@users.noreply.github.com>
2018-10-09 20:38:09 +0530

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,21 +1,23 @@
 
 function guess(){
-	    true_ans=$(ls -l |grep "^-"|wc -l)
-	        while true;
-			    do
-				            echo "please enter your guess"
-					            read  number
-						            if [ $number -lt $true_ans ]
-								            then
-										                echo "your guess is Less then the true number"
-												        elif [ $number -gt $true_ans ]
-														        then
-																            echo "your guess is Greater then the true number"
-																	            else
-																			                echo " congratulations!!!,You are right!"
-																					        break;
-																						        fi
-																							    done
-																						    }
+    true_ans=$(ls -l |grep "^-"|wc -l)
+    while true;
+    do
+        echo "please enter your guess"
+        read  number
+        if [ $number -lt $true_ans ]
+        then
+            echo "your guess is Less then the true number"
+        elif [ $number -gt $true_ans ]
+        then
+            echo "your guess is Greater then the true number"
+        else
+            echo " congratulations!!!,You are right!"
+        break;
+        fi
+    done
+}
+echo "guess the files number in the current directory!"
+guess							    }
 																						    echo "guess the files number in the current directory!"
 																						    guess
